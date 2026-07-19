234736f00d80de6289b13febb33ed06358c532e3
nplrajk <nplrajk@gmail.com>
2026-07-19 11:26:44 +0545

    cal: Include new JDN, TAI, and BS sub-modules
    Modify lib/cal/cal.asm wrapper.
    - Add include directives for gregorian.asm, unix.asm, iso8601.asm, jdn.asm, tai.asm, and bs.asm.

--- lib/cal/cal.asm
+++ lib/cal/cal.asm
@@ -15,5 +15,8 @@
 %include "lib/cal/gregorian.asm"
 %include "lib/cal/unix.asm"
 %include "lib/cal/iso8601.asm"
+%include "lib/cal/jdn.asm"
+%include "lib/cal/tai.asm"
+%include "lib/cal/bs.asm"
 
 %endif ; IO_CAL_ASM
