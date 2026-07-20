db1ee2d2bdf4957658e65caad0b4a0a0c3f489c2
nplrajk <nplrajk@gmail.com>
2026-07-20 21:55:22 +0545

    kernel/entry: remove duplicate extern smp_stacks_init in init.asm

--- kernel/entry/init.asm
+++ kernel/entry/init.asm
@@ -177,8 +177,6 @@ mm_init:
     call heap_transition
     call kmem_cache_init_all
 
-    ; 4b. Initialize Multi-Core Isolated Stacks (Subfeature 20.6)
-    extern smp_stacks_init
     call smp_stacks_init
 
     ; 5. Unmap the kernel stack guard page to trap stack overflows
