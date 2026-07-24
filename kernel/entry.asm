41ffaa86274970c10bea6e0176bbee99a6b1e03e
nplrajk <nplrajk@gmail.com>
2026-07-24 16:13:01 +0545

    feat(kernel): wire lib/ucmp/ucmp.asm master compression dispatcher into kernel entry
    Wired %include "lib/ucmp/ucmp.asm" directly into kernel/entry.asm master includes list.

--- kernel/entry.asm
+++ kernel/entry.asm
@@ -86,6 +86,7 @@ kernel_text_start:
 %include "crypto/ucrypt/ucrypt.asm"
 %include "lib/urand/urand.asm"
 %include "crypto/ux509/ux509.asm"
+%include "lib/ucmp/ucmp.asm"
 
     section .text
     global kernel_text_end
