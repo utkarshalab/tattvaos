a3ee87025abd55807ac90c7e21b0e3f6fde7a268
nplrajk <nplrajk@gmail.com>
2026-07-24 15:27:33 +0545

    feat(ucrypt): update master ucrypt.asm dispatcher API with FIPS POST initialization and 10 new modules

--- crypto/ucrypt/ucrypt.asm
+++ crypto/ucrypt/ucrypt.asm
@@ -14,10 +14,16 @@
 %include "crypto/ucrypt/guards/s2n_guard.asm"
 %include "crypto/ucrypt/guards/memory_barrier_guard.asm"
 %include "crypto/ucrypt/guards/wipe.asm"
+%include "crypto/ucrypt/ucrypt_post.asm"
 %include "crypto/ucrypt/symmetric/aes_gcm.asm"
 %include "crypto/ucrypt/symmetric/aes_gcm_4way.asm"
+%include "crypto/ucrypt/symmetric/aes_gcm_avx512.asm"
 %include "crypto/ucrypt/symmetric/aes_gcm_siv.asm"
+%include "crypto/ucrypt/symmetric/aes_ocb3.asm"
 %include "crypto/ucrypt/symmetric/aes_xts.asm"
+%include "crypto/ucrypt/symmetric/sm4_gcm.asm"
+%include "crypto/ucrypt/symmetric/aria_gcm.asm"
+%include "crypto/ucrypt/symmetric/camellia_gcm.asm"
 %include "crypto/ucrypt/symmetric/chacha20_poly1305.asm"
 %include "crypto/ucrypt/symmetric/xchacha20_poly1305.asm"
 %include "crypto/ucrypt/symmetric/aes_cbc.asm"
@@ -27,11 +33,15 @@
 %include "crypto/ucrypt/symmetric/aes_kw_ad.asm"
 %include "crypto/ucrypt/asymmetric/x25519.asm"
 %include "crypto/ucrypt/asymmetric/curve448.asm"
+%include "crypto/ucrypt/asymmetric/ed448.asm"
 %include "crypto/ucrypt/asymmetric/ecdh_p256.asm"
 %include "crypto/ucrypt/asymmetric/rsa_oaep.asm"
+%include "crypto/ucrypt/asymmetric/bip32_hdkey.asm"
+%include "crypto/ucrypt/asymmetric/secp256k1_schnorr.asm"
 %include "crypto/ucrypt/mac/hmac/hmac.asm"
 %include "crypto/ucrypt/mac/kmac/kmac.asm"
 %include "crypto/ucrypt/mac/cmac/cmac.asm"
+%include "crypto/ucrypt/mac/vmac/vmac.asm"
 %include "crypto/ucrypt/mac/poly1305/poly1305.asm"
 %include "crypto/ucrypt/mac/poly1305/poly1305_2way.asm"
 
@@ -43,6 +53,7 @@ section .text
 ; Output: RAX = 1
 ; -----------------------------------------------------------------------------
 ucrypt_init:
+    call ucrypt_post_run_all_kats   ; Execute FIPS 140-3 POST Known-Answer Tests
     mov rax, 1
     ret
 
