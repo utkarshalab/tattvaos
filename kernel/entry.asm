4485b41c1e79aaf0b8c24ae87516f396e3e714e5
nplrajk <nplrajk@gmail.com>
2026-07-25 11:32:25 +0545

    build(kernel): update entry.asm to include storage/ufs/ufs.asm
    Updated kernel entry include path from ufs/ufs.asm to storage/ufs/ufs.asm for clean storage category organization.

--- kernel/entry.asm
+++ kernel/entry.asm
@@ -87,7 +87,7 @@ kernel_text_start:
 %include "lib/urand/urand.asm"
 %include "crypto/ux509/ux509.asm"
 %include "lib/ucmp/ucmp.asm"
-%include "ufs/ufs.asm"
+%include "storage/ufs/ufs.asm"
 
     section .text
     global kernel_text_end
